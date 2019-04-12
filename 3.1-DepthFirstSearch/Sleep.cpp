ec63942c1a96725c074b7db2c9faf9918137ca5d
MMMMMMoSky <q94354906@gmail.com>
2019-04-12 14:33:57 +0800

    使用宏实现的跨平台的 sleep

---
+++ 3.1-DepthFirstSearch/Sleep.cpp
@@ -0,0 +1,60 @@
+/* macOS (OS X) */
+#ifdef __APPLE__
+#define USE_UNISTD
+#endif
+
+/* OS 9 */
+#ifdef macintosh
+#define USE_UNISTD
+#endif
+
+/* linux kernel */
+#ifdef __linux__
+#define USE_UNISTD
+#endif
+
+/* Windows */
+#ifdef _WIN32
+#define USE_WINDOWS
+#endif
+#ifdef _WIN64
+#define USE_WINDOWS
+#endif
+
+#ifdef USE_WINDOWS
+#ifdef USE_UNISTD
+#define IMPOSSIBLE_ERROR
+#endif
+#endif
+
+#ifdef USE_UNISTD
+#include <unistd.h>
+#endif
+
+#ifdef USE_WINDOWS
+#include <Windows.h>
+#endif
+
+#include "Sleep.h"
+#include <iostream>
+using namespace std;
+
+void Sleep::mSleep(unsigned int ms)
+{
+#ifdef IMPOSSIBLE_ERROR
+    cout << "Impossible error: Your environment is both Unix-Like and Windows?" << endl;
+#else
+
+#ifdef USE_UNISTD
+    usleep(ms * 1000);
+    return;
+#endif
+
+#ifdef USE_WINDOWS
+    Sleep(ms);
+    return;
+#endif
+
+    cerr << "Error: Your platform is not supported for Sleep::mSleep() yet" << endl;
+#endif
+}
---
+++ 3.1-DepthFirstSearch/Sleep.h
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2019 MMMMMMoSky All rights reserved.
+ * 
+ * 一个跨平台的 Sleep
+ * 参考 https://www.cnblogs.com/foohack/p/5013272.html
+ * 目前在以下系统可以正常使用:
+ *  Mac OS 9                macintosh Macintosh
+ *  Mac OS X (macOS)        __APPLE__ __MACH__
+ *  Linux kernel            __linux__
+ *  Windows                 _WIN32 _WIN64
+ */
+
+class Sleep
+{
+  public:
+    // 单位: 毫秒
+    static void mSleep(unsigned int ms);
+};
